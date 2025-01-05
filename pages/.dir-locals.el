9304b415887ca61ce8a44ddfdfb4fdd8d7df0838
chrischen <chrischen@ignity.xyz>
2025-01-05 18:26:39 +0800

    use org-roam with multi-directory way

---
+++ pages/.dir-locals.el
@@ -0,0 +1,9 @@
+;;; .dir_locals.el --- Description -*- lexical-binding: t; -*-
+((nil . ((eval . (setq-local
+                  org-roam-directory (expand-file-name (locate-dominating-file
+                                                        default-directory ".dir-locals.el"))))
+         (eval . (setq-local
+                  org-roam-db-location (expand-file-name ".cache/org-roam.db"
+                                                         org-roam-directory))))))
+
+;;; .dir_locals.el ends here
