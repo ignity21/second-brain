cad74c2481927e1b653725a47e3486e0507c45b1
chrischen <chrischen@ignity.xyz>
2025-01-07 14:19:48 +0800

    move notes to DataScience category

--- roamnotes/DataScience/.dir-locals.el
+++ roamnotes/DataScience/.dir-locals.el
@@ -4,6 +4,12 @@
                                                         default-directory ".dir-locals.el"))))
          (eval . (setq-local
                   org-roam-db-location (expand-file-name ".cache/org-roam.db"
-                                                         org-roam-directory))))))
+                                                         org-roam-directory)))
+         (org-publish-project-alist . (("second-brain"
+                                        :base-directory "."
+                                        :publishing-function org-html-publish-to-html
+                                        :publishing-directory "../public"
+                                        :recursive t)))
+         )))
 
 ;;; .dir_locals.el ends here
