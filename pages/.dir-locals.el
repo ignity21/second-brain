48e9214a74758a6d1724ca353c0fb4ac831cd5c9
chrischen <chrischen@ignity.xyz>
2025-02-02 17:14:01 +0800

    reorganize note directories

--- pages/.dir-locals.el
+++
@@ -1,9 +0,0 @@
-;;; .dir_locals.el --- Description -*- lexical-binding: t; -*-
-((nil . ((eval . (setq-local
-                  org-roam-directory (expand-file-name (locate-dominating-file
-                                                        default-directory ".dir-locals.el"))))
-         (eval . (setq-local
-                  org-roam-db-location (expand-file-name ".cache/org-roam.db"
-                                                         org-roam-directory))))))
-
-;;; .dir_locals.el ends here
